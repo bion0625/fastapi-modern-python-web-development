7f95c7f9661478c8d63a48fd7901a35919fd540c
bion0625 <bion0625@gmail.com>
2025-05-10 22:37:19 +0900

    3.2 FastAPI 애플리케이션

---
+++ hello.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+@app.get("/hi")
+def greet():
+    return "Hello? world?"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("hello:app", reload=True)
