199b911c02ac6f1f19e729da2215bf741244c2a5
bion0625 <bion0625@gmail.com>
2025-05-10 22:54:31 +0900

    3.3.4 헤더

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Header
 
 app = FastAPI()
-@app.post("/hi")
-def greet(who: str = Body(embed=True)):
+
+@app.get("/hi")
+def greet(who: str = Header()):
     return f"Hello? {who}?"
+
+@app.get("/agent")
+def get_agent(user_agent: str = Header()):
+    return user_agent
 
 if __name__ == "__main__":
     import uvicorn
