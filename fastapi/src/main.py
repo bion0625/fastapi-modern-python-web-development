4ceffdabfd61617e2ab6284a1e2f248d330cbb39
bion0625 <bion0625@gmail.com>
2025-05-16 21:04:42 +0900

    11.5.8 최상위 계층

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from web import creature, explorer
+from web import creature, explorer, user
 
 app = FastAPI()
 
 app.include_router(explorer.router)
 app.include_router(creature.router)
+app.include_router(user.router)
 
 @app.get("/")
 def top():
