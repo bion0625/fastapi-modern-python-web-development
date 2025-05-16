54749f01b7b277c15a93f2c0dbffbaa63a6699c3
bion0625 <bion0625@gmail.com>
2025-05-16 21:22:24 +0900

    11.7.1 CORS

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from web import creature, explorer, user
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ui.cryptids.com"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
 
 app.include_router(explorer.router)
 app.include_router(creature.router)
