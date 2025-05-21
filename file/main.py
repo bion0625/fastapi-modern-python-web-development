4c7e32546954ef3fbdc5607aa54745644b6a887e
bion0625 <stxtory@gmail.com>
2025-05-21 13:15:16 +0900

    15.2.2 UploadFile

--- file/main.py
+++ file/main.py
@@ -1,7 +1,11 @@
-from fastapi import FastAPI, File
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 @app.post("/small")
 async def upload_file(small_file: bytes = File()) -> str:
     return f"file size: {len(small_file)}"
+
+@app.post("/big")
+async def upload_bog_file(big_file: UploadFile) -> str:
+    return f"file size: {big_file.size}, name: {big_file.filename}"
