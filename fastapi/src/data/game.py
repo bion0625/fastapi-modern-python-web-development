b025d2e4efa4d527ba6bf2227fa39623ccdbe852
bion0625 <bion0625@gmail.com>
2025-05-24 14:57:41 +0900

    게임 커스텀

--- fastapi/src/data/game.py
+++ fastapi/src/data/game.py
@@ -1,4 +1,10 @@
 from data.init_db import curs
+import random
+import string
+import re
+
+def random_english(length: int) -> str:
+    return ''.join(random.choices(string.ascii_lowercase, k=length))
 
 def get_word() -> str:
     qry = "select name from creature order by random() limit 1"
@@ -6,6 +12,8 @@ def get_word() -> str:
     row = curs.fetchone()
     if row:
         name = row[0]
+        if not re.fullmatch(r'[a-zA-Z]+', name):
+            name = random_english(len(name))
     else:
         name = "bigfoot"
-    return name
+    return name.lower()
